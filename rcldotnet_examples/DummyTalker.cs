5916220b5a108fe49d439bd2afcc564ae6279291
WARREN <wrog0003@student.monash.edu>
2022-01-31 19:52:17 +1100

    Basic Start

--- rcldotnet_examples/DummyTalker.cs
+++ rcldotnet_examples/DummyTalker.cs
@@ -14,36 +14,53 @@ namespace ConsoleApplication
 { //namespace needs changing right 
     public class DummyTalker
     {
+        private bool start = true;
+        private const int numDrones = 5;
+        private int[,] startSet = new int[3, numDrones];
+        public int mapHieght = 60;
+        public int mapWidth = 50;
+        Random rnd = new Random();
+        for (int i = 0; i < numDrones; i++) 
+            {
+                startSet[1, i] = rnd.Next(1,mapHieght);
+                startSet[2, i] = rnd.Next(1,mapWidth);
+            }
         public static void Main(string[] args)
         {
-            /*
-              RCLdotnet.Init();
+            RCLdotnet.Init();
 
-              INode node = RCLdotnet.CreateNode("talker");
+            INode node = RCLdotnet.CreateNode("talker");
 
-              IPublisher<girbal_msgs.msg.StateArray> chatter_pub = node.CreatePublisher<girbal_msgs.msg.StateArray>("chatter");
+            IPublisher<girbal_msgs.msg.StateArray> chatter_pub = node.CreatePublisher<girbal_msgs.msg.StateArray>("chatter");
 
-              girbal_msgs.msg.StateArray msg = new girbal_msgs.msg.StateArray(); //create the data 
+            girbal_msgs.msg.StateArray msg = new girbal_msgs.msg.StateArray(); //create the data 
 
-              int i = 1;
+            int i = 1;
 
-              while (RCLdotnet.Ok())
-              {
-                  foreach (PropertyDescriptor descriptor in TypeDescriptor.GetProperties(msg))
-                  {
-                      string name = descriptor.Name; //what does this do?
-                      object value = descriptor.GetValue(msg);
-                      Console.WriteLine("{0}={1}", name, value); //not needed?
-                  }
-                  msg.X = i; //raw data needs to be entered here 
-                  i++;
-                  Console.WriteLine("Publishing: \"" + msg.X + "\"");
-                  chatter_pub.Publish(msg); //publishing here
+            while (RCLdotnet.Ok()) {
+                if (start) 
+                {
+                    string name = descriptor.Name;
+                 }
+            
+            else { 
 
-                  // Sleep a little bit between each message
-                  Thread.Sleep(1000);
-              }
-              */
-        }
+                foreach (PropertyDescriptor descriptor in TypeDescriptor.GetProperties(msg)) { 
+             
+                string name = descriptor.Name; //what does this do?
+                object value = descriptor.GetValue(msg);
+                Console.WriteLine("{0}={1}", name, value); //not needed?
+                }
+            }
+          msg.X = i; //raw data needs to be entered here 
+          i++;
+          Console.WriteLine("Publishing: \"" + msg.X + "\"");
+          chatter_pub.Publish(msg); //publishing here
+
+          // Sleep a little bit between each message
+          Thread.Sleep(1000);
+      }
+      
+}
     }
 }
